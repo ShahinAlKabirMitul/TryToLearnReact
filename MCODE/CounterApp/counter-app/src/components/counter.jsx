643cee4ec71c2a9a675031fafdfbac182be7052c
shahinalkabirmitul <mitul.cse@gmail.com>
2018-07-13 19:35:08 +0600

    Passing Event Arguments

--- MCODE/CounterApp/counter-app/src/components/counter.jsx
+++ MCODE/CounterApp/counter-app/src/components/counter.jsx
@@ -2,19 +2,42 @@ import React, { Component } from 'react';
 class Counter extends Component {
   state = {
     count: 0,
-    tags: [],
+  };
+  // constructor() {
+  //   super();
+  //   this.handleIncrement = this.handleIncrement.bind(this);
+  // }
+  handleIncrement = product => {
+    console.log(product);
+    this.setState({
+      count: this.state.count + 1,
+    });
   };
 
-  renderTags() {
-   
-    if (this.state.tags.length === 0) return <p>There is no tags!</p>;
-    return <ul>{this.state.tags.map(tag => <li key={tag}>{tag}</li>)}</ul>;
-  }
   render() {
-    return <div>
-    {this.state.tags.length === 0 && 'Please create a new tag'}
-    {this.renderTags()}
-    </div>;
+    return (
+      <div>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button
+          onClick={() => this.handleIncrement({ id: 5 })}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
+      </div>
+    );
+  }
+
+  getBadgeClasses() {
+    let classes = 'badge m-2 badge-';
+    classes += this.state.count === 0 ? 'warning' : 'primary';
+    return classes;
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    console.log(count);
+    return count === 0 ? 'Zero' : count;
   }
 }
 
