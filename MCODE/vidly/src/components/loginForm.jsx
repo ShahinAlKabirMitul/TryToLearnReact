8fd4b301486c753e204fcecd65a11141a7e8cc46
shahinalkabirmitul <mitul.cse@gmail.com>
2018-07-21 14:27:36 +0600

    Handling Multiple Inputs

--- MCODE/vidly/src/components/loginForm.jsx
+++ MCODE/vidly/src/components/loginForm.jsx
@@ -1,22 +1,46 @@
 import React, { Component } from 'react';
 class LoginForm extends Component {
+  state = {
+    account: { userName: '', password: '' },
+  };
   handleSubmit = e => {
     e.preventDefault();
     // call the server
     console.log('form submitted');
   };
+  handleChnage = ({ currentTarget: input }) => {
+    const account = { ...this.state.account };
+    account[input.name] = input.value;
+    this.setState({ account });
+  };
   render() {
+    const { account } = this.state;
     return (
       <div>
         <h1>Login</h1>
         <form onSubmit={this.handleSubmit}>
           <div className="form-group">
             <label htmlFor="userName">User Name</label>
-            <input id="userName" type="text" className="form-control" />
+            <input
+              value={account.userName}
+              onChange={this.handleChnage}
+              name="userName"
+              autoFocus
+              id="userName"
+              type="text"
+              className="form-control"
+            />
           </div>
           <div className="form-group">
             <label htmlFor="password">Password</label>
-            <input id="password" type="password" className="form-control" />
+            <input
+              id="password"
+              value={account.password}
+              onChange={this.handleChnage}
+              name="password"
+              type="password"
+              className="form-control"
+            />
           </div>
           <button className="btn btn-primary">Login</button>
         </form>
