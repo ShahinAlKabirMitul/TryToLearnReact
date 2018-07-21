17ef980a59214c0b6fafcb48371d2309ea326c78
shahinalkabirmitul <mitul.cse@gmail.com>
2018-07-21 22:20:07 +0600

    Disabling the Submit Button

--- MCODE/vidly/src/components/loginForm.jsx
+++ MCODE/vidly/src/components/loginForm.jsx
@@ -72,7 +72,9 @@ class LoginForm extends Component {
             onChnage={this.handleChnage}
             error={errors.password}
           />
-          <button className="btn btn-primary">Login</button>
+          <button disabled={this.validate()} className="btn btn-primary">
+            Login
+          </button>
         </form>
       </div>
     );
