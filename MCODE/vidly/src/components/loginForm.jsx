a9c68cec523b2ed870baae98732ee297952d223c
shahinalkabirmitul <mitul.cse@gmail.com>
2018-07-21 12:21:21 +0600

    Building a Bootstrap  login Form

---
+++ MCODE/vidly/src/components/loginForm.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+class LoginForm extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Login</h1>
+        <form>
+          <div className="form-group">
+            <label htmlFor="userName">User Name</label>
+            <input id="userName" type="text" className="form-control" />
+          </div>
+          <div className="form-group">
+            <label htmlFor="password">Password</label>
+            <input id="password" type="password" className="form-control" />
+          </div>
+          <button className="btn btn-primary">Login</button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default LoginForm;
