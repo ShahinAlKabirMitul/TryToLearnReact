17e05b46554c8bdba418e65d6bff113d3293d4de
shahinalkabirmitul <mitul.cse@gmail.com>
2018-08-11 15:43:18 +0600

    pass jwt header

--- MCODE/vidly/src/services/authService.js
+++ MCODE/vidly/src/services/authService.js
@@ -4,6 +4,7 @@ import { apiUrl } from '../config.json';
 const apiEndPoint = apiUrl + '/auth';
 
 const tokenKey = 'token';
+http.setJwt(getJwt());
 export async function login(email, password) {
   const { data: jwt } = await http.post(apiEndPoint, { email, password });
   localStorage.setItem(tokenKey, jwt);
@@ -15,6 +16,9 @@ export function logout() {
   localStorage.removeItem(tokenKey);
 }
 
+export function getJwt() {
+  return localStorage.getItem(tokenKey);
+}
 export function getCurrentUser() {
   try {
     const jwt = localStorage.getItem(tokenKey);
@@ -29,4 +33,5 @@ export default {
   logout,
   getCurrentUser,
   loginWithJwt,
+  getJwt,
 };
