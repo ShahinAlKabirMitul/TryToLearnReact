3aa127db9100692dc131dc5ca408da89014b6940
shahinalkabirmitul <mitul.cse@gmail.com>
2018-08-15 08:47:35 +0600

    add env

--- MCODE/vidly/src/index.js
+++ MCODE/vidly/src/index.js
@@ -7,6 +7,7 @@ import registerServiceWorker from './registerServiceWorker';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'font-awesome/css/font-awesome.css';
 
+console.log("Superman",process.env.REACT_APP_NAME);
 ReactDOM.render(
   <BrowserRouter>
     <App />
